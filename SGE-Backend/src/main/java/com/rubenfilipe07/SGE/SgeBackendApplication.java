a56007b08f9dfc987e228c1f446ac8061e84c7d8
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-09-10 17:20:03 -0300

    feat: sending spring initial files

---
+++ SGE-Backend/src/main/java/com/rubenfilipe07/SGE/SgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rubenfilipe07.SGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeBackendApplication.class, args);
+	}
+
+}
---
+++ SGE-Backend/src/test/java/com/rubenfilipe07/SGE/SgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenfilipe07.SGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
