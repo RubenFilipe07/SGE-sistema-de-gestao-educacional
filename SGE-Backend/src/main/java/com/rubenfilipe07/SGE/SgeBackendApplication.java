1da4f01fd556398aac83f13878871bdbebed0d8b
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-09-10 21:56:12 -0300

    feat(api): add swagger-ui (springdoc.org)

--- SGE-Backend/src/main/java/com/rubenfilipe07/SGE/SgeBackendApplication.java
+++ SGE-Backend/src/main/java/com/rubenfilipe07/SGE/SgeBackendApplication.java
@@ -3,7 +3,12 @@ package com.rubenfilipe07.SGE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="SGE - Sistema de gestao educacional", version = "1", description = "Uma API REST que tem como objetivo gerenciar os alunos matriculados em uma turma."))
+
 public class SgeBackendApplication {
 
 	public static void main(String[] args) {
--- SGE-Backend/src/main/java/com/rubenfilipe07/SGE/controllers/AlunoController.java
+++ SGE-Backend/src/main/java/com/rubenfilipe07/SGE/controllers/AlunoController.java
@@ -9,28 +9,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rubenfilipe07.SGE.models.AlunoModel;
 import com.rubenfilipe07.SGE.repositories.AlunoRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import com.rubenfilipe07.SGE.dtos.AlunoRecordDto;
 
 @RestController
+@RequestMapping(value = "/", produces = {"application/json"})
+@Tag(name = "Alunos")
 public class AlunoController {
 
 	@Autowired
 	AlunoRepository alunoRepository;
 
 	@PostMapping("/alunos")
+	@Operation(summary ="Cadastra alunos", method = "POST")
+	@ApiResponses(value = {
+	    @ApiResponse(responseCode = "200", description = "Aluno encontrado"),
+	    @ApiResponse(responseCode = "404", description = "Aluno não encontrado")
+	})
 	public ResponseEntity<AlunoModel> saveProduct(@RequestBody @Valid AlunoRecordDto alunoRecordDto) {
 		var alunoModel = new AlunoModel();
 		BeanUtils.copyProperties(alunoRecordDto, alunoModel);
@@ -38,10 +51,11 @@ public class AlunoController {
 	}
 
 	@GetMapping("/alunos")
+	@Operation(summary ="Retorna uma lista de todos os alunos cadastrados", method = "GET")
 	public ResponseEntity<List<AlunoModel>> getAllAlunos() {
 		return ResponseEntity.status(HttpStatus.OK).body(alunoRepository.findAll());
 	}
-
+	@Operation(summary ="Retorna um único aluno", method = "GET")
 	@GetMapping("/alunos/{id}")
 	public ResponseEntity<Object> getOneAluno(@PathVariable(value = "id") UUID id) {
 		Optional<AlunoModel> aluno0 = alunoRepository.findById(id);
@@ -53,7 +67,7 @@ public class AlunoController {
 		}
 	}
 	
-
+	@Operation(summary ="Atualiza um único aluno", method = "PUT")
 	@PutMapping("/alunos/{id}")
 	public ResponseEntity<Object> setOneAluno(@PathVariable(value = "id") UUID id, @RequestBody @Valid AlunoRecordDto alunoRecordDto) {
 		Optional<AlunoModel> aluno0 = alunoRepository.findById(id);
@@ -67,6 +81,7 @@ public class AlunoController {
 		}
 	}
 	
+	@Operation(summary ="Deleta um único aluno", method = "DELETE")
 	@DeleteMapping("/alunos/{id}")
 	public ResponseEntity<Object> deleteOneAluno(@PathVariable(value = "id") UUID id) {
 		Optional<AlunoModel> aluno0 = alunoRepository.findById(id);
